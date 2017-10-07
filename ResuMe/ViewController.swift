ec9b13a0975dcba1f78246b33c6efb6ca376fb04
Patrick Chao <patrickrchao@gmail.com>
2017-10-07 01:32:42 -0700

    First commitgit status not really second commit ay lmoa

--- ResuMe/ViewController.swift
+++ ResuMe/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var WelcomeLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBAction func loginButton(_ sender: UIButton) {
+        if emailTextField.text == "pat" && passwordTextField.text=="pass"{
+           self.performSegue(withIdentifier: "LoginSegue", sender: self)
+        }
+        
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
