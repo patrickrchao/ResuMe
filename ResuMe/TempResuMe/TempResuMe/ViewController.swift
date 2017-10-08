92a093a75d53e198191d3c8b1acbcf39c459efbc
Hannah He <hahe@berkeley.edu>
2017-10-08 01:30:02 -0700

    finished copy

---
+++ ResuMe/TempResuMe/TempResuMe/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TempResuMe
+//
+//  Created by Hannah He on 10/7/17.
+//  Copyright © 2017 Hannah He. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.dropbox.com/s/33n2ycp1gt0u0pc/hw2.pdf")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
